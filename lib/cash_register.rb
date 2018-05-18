396bf909e77626b225092c82bd2bd6dc97baa957
cindych419@gmail.com <cindych419@gmail.com>
2018-05-18 21:30:47 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,9 +14,9 @@ def add_item(title, price, quantity=1)
   quantity.times do
     @titles<<title
   end
-  @total+=price*quantity
+  @total=@total+@last_transaction
 
-  @last_transaction=[]
+  @last_transaction=price*quantity
   #put title into an titles array
 #calculate the new total include total items and total price
 end
