e592cec75867655366521a51219644e0bfdbf174
cindych419@gmail.com <cindych419@gmail.com>
2018-05-18 21:16:43 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ attr_accessor :total, :discount, :titles
   end
 
 def add_item(title, price, quantity=1)
-  @titles<<title
+  @titles<<title*quantity
   @total+=price*quantity
 
 #put title into an titles array
